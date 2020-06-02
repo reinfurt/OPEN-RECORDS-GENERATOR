b9729438674f15e38a399329deed6768194bc050
david reinfurt <reinfurt@o-r-g.com>
2020-06-02 11:58:41 -0400

    selective merge updates from master

--- views/info.php
+++ views/info.php
@@ -3,19 +3,21 @@
 // fopen wrappers?
 // see 'notes' on this page:
 // http://php.net/manual/en/function.file-get-contents.php
-$license_file = $admin_root."static/gnu.txt";
+$license_file = $admin_root."static/license.txt";
 $license = file_get_contents($license_file);
+$license_gnu_file = $admin_root."static/gnu.txt";
+$license_gnu = file_get_contents($license_gnu_file);
 ?><div id="body-container">
 	<div id="body">
 		<div class="self-container">
 			<p>
 				Open Records Generator<br>
-				Version 3.0.0<br>
-				18 May 2020<br>
 				<a href="http://www.o-r-g.com/" target="_blank">O R G inc.</a>
+                <br/><br/>
+                Licensed to: <? echo nl2br($license); ?>
 			</p>
 			<div class="license-container">
-				<textarea class="large"><? echo $license; ?></textarea>
+				<textarea class="large"><? echo $license_gnu; ?></textarea>
 			</div>
 			<div>
 				<a href="<? echo $js_back; ?>">&lt; RETURN</a>
